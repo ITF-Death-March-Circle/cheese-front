52194f57989ce4edc20f0d260307e6c52b561a3c
yukinissie <bigtree2490@outlook.jp>
2021-09-10 00:18:40 +0900

    デプロイできるかも

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const consola = require('consola')
 const { loadNuxt } = require('nuxt-start')
 const app = express()
 
+const isDev = false
+
 async function start() {
   const nuxt = await loadNuxt(isDev ? 'dev' : 'start')
   await nuxt.listen(process.env.PORT, process.env.HOST)
