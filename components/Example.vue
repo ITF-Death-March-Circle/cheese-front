95a9b00786508cf19464c10399f63fbc7bcca95d
yukinissie <bigtree2490@outlook.jp>
2021-09-15 20:54:08 +0900

    update file for deploy

--- components/Example.vue
+++ components/Example.vue
@@ -51,6 +51,23 @@
     </ul>
     <img src="~/assets/image/Penguin.png" alt="penguin" class="penguin">
   </div>
+  <div class="example">
+    <ul class="example__list">
+      <li class="example__item">
+        <img src="~/assets/image/36b2e96791f5c36875bfcbc2e922745f.png" alt="ぺんぎんちゃん">
+        <p>ぺんぎんちゃん</p>
+      </li>
+      <li class="example__item">
+        <img src="~/assets/image/00000422.png" alt="ぺんぎんさん">
+        <p>ぺんぎんさん</p>
+      </li>
+      <li class="example__item">
+        <img src="~/assets/image/illustkun-04058-penguin.png" alt="ぺんぎんくん">
+        <p>ぺんぎんくん</p>
+      </li>
+    </ul>
+    <img src="~/assets/image/Penguin.png" alt="penguin" class="penguin">
+  </div>
 </div>
 </template>
 
