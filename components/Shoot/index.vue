b72ccaa350c1734e05793188f905ce812422bd6d
yuki nishi <38881185+yukinissie@users.noreply.github.com>
2021-09-19 22:08:48 +0900

    Revert "Revert "WIP: プロトタイプの作成""

---
+++ components/Shoot/index.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="shoot">
+    <div class="row">
+      <div class="col-md-6">
+        <h2>Current Camera</h2>
+        <code v-if="device">{{ device.label }}</code>
+        <div class="border">
+          <vue-web-cam
+            ref="webcam"
+            :device-id="deviceId"
+            width="100%"
+            @started="onStarted"
+            @stopped="onStopped"
+            @error="onError"
+            @cameras="onCameras"
+            @camera-change="onCameraChange"
+          />
+        </div>
+
+        <div class="row">
+          <div class="col-md-12">
+            <select v-model="camera">
+              <option>-- Select Device --</option>
+              <option
+                v-for="d in devices"
+                :key="d.deviceId"
+                :value="d.deviceId"
+              >{{ d.label }}</option>
+            </select>
+          </div>
+          <div class="col-md-12">
+            <button type="button" class="btn btn-primary" @click="onCapture">Capture Photo</button>
+            <button type="button" class="btn btn-danger" @click="onStop">Stop Camera</button>
+            <button type="button" class="btn btn-success" @click="onStart">Start Camera</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <h2>Captured Image</h2>
+        <figure class="figure">
+          <img :src="img" class="img-responsive" />
+        </figure>
+      </div>
+
+      <a ref="download" @click="download">撮影元データダウンロード</a>
+      <!-- <nuxt-img src="https://dmc-cheese.imgix.net/image/image02.png" class="img-responsive" /> -->
+    </div>
+  </div>
+</template>
+
+
+<script lang="js">
+/* eslint-disable object-shorthand */
+/* eslint-disable no-console */
+import moment from 'moment';
+
+export default {
+  data() {
+    return {
+      img: null,
+      camera: null,
+      deviceId: null,
+      devices: []
+    };
+  },
+  computed: {
+    device: function() {
+      return this.devices.find(n => n.deviceId === this.deviceId);
+    }
+  },
+  watch: {
+    camera: function(id) {
+      this.deviceId = id;
+    },
+    devices: function() {
+      // Once we have a list select the first one
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [first, ...tail] = this.devices;
+      if (first) {
+        this.camera = first.deviceId;
+        this.deviceId = first.deviceId;
+      }
+    }
+  },
+  methods: {
+    download() {
+      const a = this.$refs.download;
+      // Base64文字列をhrefへセット
+      a.href = this.img;
+      // ダウンロード時のファイル名を指定
+      const now = moment(new Date()).format('YYYY-MM-DD_hh-mm-ss');
+      a.download = `${now}.jpg`;
+    },
+    onCapture() {
+      this.img = this.$refs.webcam.capture();
+      this.$router.push('aftershoot')
+    },
+    onStarted(stream) {
+      console.log("On Started Event", stream);
+    },
+    onStopped(stream) {
+      console.log("On Stopped Event", stream);
+    },
+    onStop() {
+      this.$refs.webcam.stop();
+    },
+    onStart() {
+      this.$refs.webcam.start();
+    },
+    onError(error) {
+      console.log("On Error Event", error);
+    },
+    onCameras(cameras) {
+      this.devices = cameras;
+      console.log("On Cameras Event", cameras);
+    },
+    onCameraChange(deviceId) {
+      this.deviceId = deviceId;
+      this.camera = deviceId;
+      console.log("On Camera Change Event", deviceId);
+    }
+  }
+};
+</script>
