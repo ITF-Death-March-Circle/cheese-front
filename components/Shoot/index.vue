ea770426ed3c0c2fae5acbf4eccb190d4da77526
i15317 <30828280+notchman8600@users.noreply.github.com>
2021-09-20 21:05:56 +0900

    敲きを作成

--- components/Shoot/index.vue
+++ components/Shoot/index.vue
@@ -44,13 +44,14 @@
 
       <a ref="download" @click="download">撮影元データダウンロード</a>
       <!-- <nuxt-img src="https://dmc-cheese.imgix.net/image/image02.png" class="img-responsive" /> -->
+
     </div>
   </div>
 </template>
-
-
 <script lang="js">
-/* eslint-disable object-shorthand */
+/* eslint-disable
+
+ object-shorthand */
 /* eslint-disable no-console */
 import moment from 'moment';
 
@@ -83,6 +84,16 @@ export default {
     }
   },
   methods: {
+    upload(){
+      const imageFile = new File([this.img], "hogehoge.jpg", {type: "image/jpg"});
+      const params = new FormData();
+      params.append("image",imageFile)
+      this.$axios.$post("http://localhost:8080/upload",params)
+        .then(res=>{
+          console.log(res)
+          alert("送信完了")
+        })
+    },
     download() {
       const a = this.$refs.download;
       // Base64文字列をhrefへセット
@@ -94,6 +105,7 @@ export default {
     onCapture() {
       this.img = this.$refs.webcam.capture();
       this.$router.push('aftershoot')
+      this.upload()
     },
     onStarted(stream) {
       console.log("On Started Event", stream);
